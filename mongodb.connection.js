99d6c59cbe22343bf9ed3cc52a13fd443903182b
master2024@prz.edu.pl <master2024@prz.edu.pl>
2024-02-27 11:12:33 +0100

    cDB2

--- mongodb.connection.js
+++ mongodb.connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
 //const certPath = '/Users/tomaszrak/MERN2024/mearn2024s/X509-cert-2405989301677726198.pem';
-const certPath = 'X509-cert-2405989301677726198.pem';
+const certPath = './X509-cert-2405989301677726198.pem';
 
 const uri = process.env.MONGODB_URI || 'mongodb+srv://cluster0.uzeo7xt.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority';
 
