d9c602b2968640c655490aa10840058847b9888e
master2024@prz.edu.pl <master2024@prz.edu.pl>
2024-02-27 13:23:57 +0100

    cDB6

--- mongodb.connection.js
+++ mongodb.connection.js
@@ -1,25 +1,18 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+//const certPath = '/Users/tomaszrak/MERN2024/mearn2024s/X509-cert-2405989301677726198.pem';
+const certPath = './X509-cert-2405989301677726198.pem';
 
-// Dekodowanie certyfikatu z zmiennej środowiskowej
-let cert;
-if (process.env.MONGODB_CERT) {
-  cert = Buffer.from(process.env.MONGODB_CERT, 'base64').toString('utf-8');
-}
-
-const uri = process.env.MONGODB_URI || 'mongodb+srv://cluster0.uzeo7xt.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority'; // Upewnij się, że masz zmienną środowiskową MONGODB_URI
+const uri = process.env.MONGODB_URI || 'mongodb+srv://cluster0.uzeo7xt.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority';
 
 mongoose.set('strictQuery', false);
 
-const connectToMongoDB = () => {
-  mongoose.connect(uri, {
-    tls: true,
-    tlsCertificateKeyFile: cert ? './X509-cert-2405989301677726198.pem' : undefined, // Tutaj użyjemy zmiennej cert
-    // Możesz potrzebować dodatkowej konfiguracji w zależności od twojego połączenia
-  }).then(() => console.log('Connected to MongoDB......'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
-};
-
 module.exports = connectToMongoDB;
 
-
+function connectToMongoDB() {
+    mongoose.connect(uri, {
+      tls: true,
+      tlsCertificateKeyFile: certPath,
+    }).then(() => console.log('Connected to MongoDB......'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+}
