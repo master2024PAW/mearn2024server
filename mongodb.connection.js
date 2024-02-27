4e8c96e60d7e53898690a89a3a047a02c7bd457a
master2024@prz.edu.pl <master2024@prz.edu.pl>
2024-02-27 11:53:00 +0100

    cDB4

--- mongodb.connection.js
+++ mongodb.connection.js
@@ -14,7 +14,7 @@ mongoose.set('strictQuery', false);
 const connectToMongoDB = () => {
   mongoose.connect(uri, {
     tls: true,
-    tlsCAFile: cert ? './X509-cert-2405989301677726198.pem' : undefined, // Tutaj użyjemy zmiennej cert
+    tlsCertificateKeyFile: cert ? './X509-cert-2405989301677726198.pem' : undefined, // Tutaj użyjemy zmiennej cert
     // Możesz potrzebować dodatkowej konfiguracji w zależności od twojego połączenia
   }).then(() => console.log('Connected to MongoDB......'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
